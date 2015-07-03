7e0150a781cd2fa45d6fdc599180f54f370a93eb
cacayperez <cacayperez@outlook.com>
2015-07-04 05:43:35 +0800

    fixed modal scrolling by updating bootstrap

--- js/classie.js
+++ js/classie.js
@@ -1,7 +1,7 @@
 /*!
  * classie - class helper functions
  * from bonzo https://github.com/ded/bonzo
- * 
+ *
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
@@ -11,7 +11,7 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-( function( window ) {
+var classiejs = ( function( window ) {
 
 'use strict';
 
